de5a83148d1a092a01842c234eb4d2e42fc9a500
Gervel Giva <gervelgiva@gmail.com>
2017-07-20 16:09:43 +0800

    Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,37 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { RouterModule }         from '@angular/router';
+import { HttpModule }           from '@angular/http';
 
-import { AppComponent }  from './app.component';
-import { HeroesComponent }  from './heroes/heroes.component';
-import { DashboardComponent }  from './dashboard/dashboard.component';
-import { HeroDetailComponent} from './hero-detail/hero-detail.component';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
-import { HeroService } from './hero/hero.service';
+import { AppComponent }         from './app.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent}   from './hero-detail/hero-detail.component';
+import { HeroSearchComponent}   from './hero-search/hero-search.component';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HeroService }          from './hero/hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule 
   ],
   declarations: [ 
     AppComponent, 
     HeroesComponent, 
     HeroDetailComponent, 
-    DashboardComponent 
+    DashboardComponent ,
+    HeroSearchComponent
   ],
   bootstrap:    [ AppComponent ],
   providers:    [HeroService]
